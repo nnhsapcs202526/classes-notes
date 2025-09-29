b695353ccb9609edd55c61db45e65d847367bd66
Matt Callaghan <mcallaghan@naperville203.org>
2025-09-29 09:20:05 -0500

    Updates to starter code

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -10,12 +10,249 @@ import java.util.Scanner;
  */
 public class CaesarCipher
 {
+    /*
+     * static: same value for the variable for all objects of the class.
+     *      This is like class attributes in Python.
+     *      Static class variables can be accessed directly through the class
+     *          (e.g., CaesarCipher.ALPHABET, Math.PI, Color.RED).
+     */
     private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
     private String keyphrase;
     
+    public CaesarCipher(String initialKeyphrase)
+    {
+        //this.keyphrase = initialKeyphrase;
+        // prepare the keyphrase by removing duplicate letters
+        this.compressKeyphrase(initialKeyphrase);
+    }
     
+    /**
+     * Returns a string that describes the average time to crack the cipher,
+     *      in several formats, based on the specified number of seconds per guess.
+     *      
+     *  @param secPerGuess the number of seconds to evaluate each attempt
+     *  @return a string that describes the average time to crack the cipher
+     */
+    public String getComplexityDescription(int secPerGuess)
+    {         
+         /*
+         * Instead of using a "magic number" (e.g., 3.14159), use constants defined
+         *      by us or the Java Standard Library. For example, in the Math class
+         *      is defined:
+         *      
+         *      public static final double PI = 3.141592654;
+         *      
+         *  Declare a constant with the final keyword.
+         *      By convention, constants are in all caps with underscores.
+         */
+        final int SECONDS_FOR_EVERY_MINUTE = 60;
+        final int MINUTES_FOR_EVERY_HOUR = 60;
+        final int HOURS_FOR_EVERY_DAY = 24;
+        final int DAYS_FOR_EVERY_YEAR = 365;
+        
+        // if we try to change the value, a compiler error will be generated
+        //SECONDS_FOR_EVERY_MINUTE = 30;
+        
+        String desc = "";
+
+        // one method in a class can invoke another method in the same class
+        //  we invoke the method on "this"
+        long totalSeconds = 0;
+        
+        /*
+         * Use integer division to calculate how many whole minutes are in the
+         *      specified number of seconds.
+         *      
+         *  Integer division (like // operator in Python) discards the remainder (truncates).
+         *  
+         *  Java does integer division when both operands are integer types;
+         *      floating-point division when one or both operands are floating-point types.
+         *      
+         *  For example:
+         *      3 / 4 => 0          (3 and 4 are int literals)
+         *      3.0 / 4 => 0.75     (3.0 is a double literal)
+         */
+        long wholeMinutes = totalSeconds / SECONDS_FOR_EVERY_MINUTE;
+        
+        /*
+         * Use the modulo (mod, remainder) operator to calculate how many seconds are
+         *      leftover.
+         *      
+         *  The mod operator (%) returns the remainder of the division operation.
+         *  
+         *  It can be very useful when paired with integer division.
+         *  
+         *  For example:
+         *      7 % 2 => 1
+         *      11 % 3 => 2
+         *      6 % 2 => 0
+         *      4 % 11 => 4
+         *      
+         *  % 2 is frequently used to test odd/even (odd => 1; even => 0)
+         */
+        long leftoverSeconds = totalSeconds % SECONDS_FOR_EVERY_MINUTE;
+        
+        long wholeHours = 0;
+        long leftoverMinutes = 0;
     
+        long wholeDays = 0;
+        long leftoverHours = 0;
+    
+        long wholeYears = 0;
+        long leftoverDays = 0;
+    
+        desc = "Average time to crack: " + wholeYears + " years, " + leftoverDays +
+        " days, " + leftoverHours + " hours, " + leftoverMinutes + " minutes, " +
+        leftoverSeconds + " seconds\n";
+
+        /*
+         * A conversion is when a data value is converted from one type to another
+         *      (e.g., int to a double; double to an int; int to a long)
+         *      
+         *  Widening: preserves information (e.g., int to a double, int to a long)
+         *  Narrowing: lossy; may lose information (e.g., double to an int)
+         *  
+         *  Java only automatically performs widening conversions.
+         */
+        double yearsAsDecimal = 0;
+        
+        /*
+         * Arithmetic Promotion
+         * 
+         *  If the two operands are of different types, Java attempts to promote one
+         *      of the operands (widening conversion) and then performs the operation.
+         *      
+         *  In this case, both SECONDS_FOR_EVERY_MINUTE and MINUTES_FOR_EVERY_HOUR are ints;
+         *      so, Java doesn't perform any promotion, and instead performs the
+         *      multiplication and returns the result as an int. Only after all three
+         *      multiplications does Java promote the int value of the resulting product
+         *      to a long and then assigns it to SECONDS_FOR_EVERY_YEAR.
+         *      
+         *  This promotion may be too late! If the multiplication overflows an int,
+         *      the wrong value will be promoted to a long and stored.
+         */
+        final long SECONDS_FOR_EVERY_YEAR = 0;
+    
+        /*
+         * In this example, the value of SECONDS_FOR_EVERY_YEAR is promoted to a double
+         *      and then floating-point division is performed and the result assigned
+         *      to yearsAsDecimal.
+         *      
+         *  The local variable SECONDS_FOR_EVERY_YEAR is still a long and still has
+         *      the same value.
+         */
+        yearsAsDecimal = yearsAsDecimal / SECONDS_FOR_EVERY_YEAR;
+        desc += "or " + yearsAsDecimal + " years\n";
+        
+        /*
+         * To force a conversion, use the cast operator.
+         *      A cast is the "I know what I'm doing, trust me" conversion.
+         *      
+         *  (int)84.69 => truncates to an int with a value of 84
+         *  (int)(3.6 + 0.5) => truncates 4.1 to an int with a value of 4
+         *  
+         *  If we want to round a double to the nearest integer value, one approach is
+         *      to add 0.5 and then cast the result to an integer, which truncates the
+         *      decimal portion.
+         *      
+         *  The following divides yearsAsDecimal by 10, then rounds the resulting long
+         *      to an int
+         */
+        int decades = 0;
+        
+        /*
+         * However, you cannot always cast a value to another type. For example,
+         *      you cannot cast a primitive value to a class type. The following
+         *      will not work as casting reinterprets a value as a different type.
+         *      Instead rely on the String concatenation operator to convert
+         *      (not cast) the primitive value to a String representation of that value.
+         */
+        //String decadesString = (String)decades;
+        desc += "or about " + decades + " decades\n";
+        
+        return desc;
+
+    }
+    
+    /**
+     * Compress the specified keyphrase by removing all duplicate letters.
+     * 
+     * @param initKeyphrase the keyphrase to compress
+     */
+    private void compressKeyphrase(String initKeyphrase)
+    {
+        this.keyphrase = "";
+        
+        /*
+         * length
+         *      returns the number of characters in the string
+         */
+        int keyphraseLength = 0;
+        
+        for(int i = 0; i < keyphraseLength; i++)
+        {
+            /*
+             * substring
+             *      returns part of the string starting at the first specified index
+             *          up to, but not including, the second specified index
+             *          
+             *  initKeyphrase:
+             *  C A E S A R
+             *  0 1 2 3 4 5             <= indicies
+             *  
+             *  length: 6
+             */
+            
+            
+            /*
+             * substring
+             *      if only one index is specified, returns part of the string starting
+             *          at the specified index through the end of the string
+             *          
+             *      substring does not support negative indicies
+             *          For example, instead of -2, we would specify
+             *              initKeyphrase.length() - 2
+             *          
+             *  initKeyphrase:
+             *  C A E S A R
+             *  0 1 2 3 4 5             <= indicies
+             *  
+             *  length: 6
+             */
+            
+            
+            /*
+             * indexOf
+             *      returns the index of the start of the first occurrence of the
+             *          specified string
+             *      if not found, returns -1
+             *      
+             *  restOfKeyphrase:
+             *  A E S A R
+             *  0 1 2 3 4               <= indicies
+             *  
+             *  length: 5
+             *  
+             *  For example, restOfKeyphrase("SA") => returns 2
+             */
+            
+            
+            /*
+             * String concatenation
+             *      + is the string concatenation operator
+             *      concatenates the second String operand to the end of the first
+             *          String operand and returns a reference to the new String
+             *      if one or both operands are a String type, + is the string
+             *          concatenation operator (operands are converted to String
+             *          objects); otherwise, + is the addition operator
+             *          
+             *  int x = 7;
+             *  String xAsString = "" + x;          // xAsString => "7"
+             */
+            
+        }
+    }
     
     
     /**
@@ -113,5 +350,36 @@ public class CaesarCipher
         // average time is half the worst time since the best time is cracking the
         //  cipher on the first attempt
         return worstCaseTimeToCrack/2;
+    }
+    
+    /**
+     * Generates a pseudorandom keyphrase of the specified length in characters
+     * 
+     * @param length the number of characters in the keyphrase
+     * @return a pseudorandom keyphrase of the specified length
+     */
+    public static String generateKeyphrase(int length)
+    {
+        String keyphrase = "";
+        
+        for(int i = 0; i < length; i++)
+        {
+            /*
+             * The Math.random static method returns a double [0.0 ... 1.0)
+             * 
+             * Often we use the following algorithm to generate random integers from
+             *      [min ... max]:
+             *      
+             *      int n = (int)((Math.random() * (max - min + 1)) + min);
+             *      
+             *  For example: generate a random int [0 ... 25]
+             *  
+             *  A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+             *  0 1 2 3 4 5 6    ...                              25    <= indices
+             */
+            
+        }
+        
+        return keyphrase;
     }
 }
--- CaesarCipherDemo.java
+++ CaesarCipherDemo.java
@@ -10,6 +10,63 @@ public class CaesarCipherDemo
 {
     public static void main(String[] args)
     {
+        System.out.println("CaesarCipher class demo:\n");
+
+	// demo of overflow error
+	
+
+	// demo of floating point imprecision
+	
+
+        /*
+         * A Scanner object parses primitive types and Strings from a stream.
+         * 
+         *  A stream is a sequence of characters from a file, String, terminal,
+         *      network connection, etc.
+         *      
+         *  Parsing is separating a sequence of characters into tokens based on
+         *      delimiters.
+         *      
+         *      A token is a meaningful sequence of characters (e.g., word).
+         *      
+         *      Delimiters are characters that separate tokens
+         *          (by default, whitespace (space, tab, newline)).
+         *          
+         *  When we create a Scanner object, we have to specify the input stream
+         *      (e.g., System.in which is the terminal input).
+         */
+        
+        
+        /*
+         * Best practices:
+         *      1. prompt the user for what you want them to input
+         *      2. use print, not println; so that the cursor is at the end of the
+         *          prompt and not on a new line
+         *      3. leave a space after the prompt
+         */
+        System.out.print("Enter the text to encrypt: ");
+        
+        /*
+         * The nextLine method returns all characters up to the end of the line
+         *      (e.g., where the user typed enter)
+         */
+
+        
+        System.out.print("Enter the keyphrase (no spaces): ");
+        
+        /*
+         * The next method returns the next token in the stream as a String
+         */
+
+        System.out.print("Enter the number of seconds to test a guessed keyphrase: ");
+        
+        /*
+         * The nextInt method attempts to convert the next token in the stream to an int
+         *      and returns the value. If the next token cannot be converted, an
+         *      exception is generated.
+         *      
+         *  The nextDouble method behaves in the same way for doubles.
+         */
         
     }
 }
